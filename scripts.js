f65aa8addc7d8c48dfb5ceb2da760aa7d288d411
Stephen Stacey <stacey_stephen@hotmail.com>
2024-04-19 21:50:48 +0100

    My first commit - commiting all files to the repository

---
+++ scripts.js
@@ -0,0 +1,27 @@
+/* Place your JavaScript in this file */
+
+// Initialize map 1
+var map1 = L.map('map1').setView([54.877826, -6.348212], 8);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map1);
+    L.marker([54.877826, -6.348212]).addTo(map1)
+      .bindPopup('This is Weekend away 1.')
+      .openPopup();
+
+
+// Initialize map 2
+var map2 = L.map('map2').setView([53.349805, -6.26031], 10);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map2);
+    L.marker([53.349805, -6.26031]).addTo(map2)
+      .bindPopup('This is Weekend away 2.')
+      .openPopup(); 
+
+var map = L.map('map3').setView([53.349805, -6.26031], 10); // Example: Dublin, Ireland
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);               
+
