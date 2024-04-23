94b8822bc346cc16e49a6406c94a815338097174
StephenStacey <stacey_stephen@hotmail.com>
2024-04-23 21:57:57 +0100

    add castlemartyr to map

--- scripts.js
+++ scripts.js
@@ -11,15 +11,15 @@ var map1 = L.map('map1').setView([54.877826, -6.348212], 8);
 
 
 // Initialize map 2
-var map2 = L.map('map2').setView([53.349805, -6.26031], 10);
+var map2 = L.map('map2').setView([51.911408, -8.063414], 8);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map2);
-    L.marker([53.349805, -6.26031]).addTo(map2)
+    L.marker([51.911408, -8.063414]).addTo(map2)
       .bindPopup('This is Weekend away 2.')
       .openPopup(); 
 
-var map = L.map('map3').setView([53.349805, -6.26031], 10); // Example: Dublin, Ireland
+var map = L.map('map3').setView([51.911408, -8.063414], 8); // Example: Castelemartyr Resort, Ireland
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
